2a44d25f193156a49d1b1659a09f31e784fee05e
GianSuneo <gian64881@gmail.com>
2026-02-02 00:44:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -104,6 +104,15 @@ def detect_voice(
             "Classification performed using trained ML model"
         ]
     }
+    if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
+
+
 
 
 
